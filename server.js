819e3250d72c10223cab17abc184d6d50e839a8a
=KayseMca <=madadmca04@gmail.com>
2020-10-19 03:42:32 -0700

    updates  to deploy to heroku

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/angularapp/crud-project'));
+app.use(express.static(__dirname + '/dist/crud-project'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/angularapp/crud-project/index.html'));
+        '/dist/crud-project/index.html'));
 });
 app.listen(process.env.PORT || 8080);
