a83a360ab63713254cda039110018aeb080ff0df
=KayseMca <=madadmca04@gmail.com>
2020-10-19 04:11:16 -0700

    updates  to deploy to heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/crud-project'));
+// app.use("/static", express.static('./static/'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
         '/dist/crud-project/index.html'));
