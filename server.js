0f620f3e37f171b87d8c541f0e67af096da54d19
=KayseMca <=madadmca04@gmail.com>
2020-10-19 22:31:39 -0700

    updates hosts for deploying heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/crud-project'));
+app.use(express.static(__dirname + '/dist/CRUD-project'));
 // app.use("/static", express.static('./static/'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/crud-project/index.html'));
+        '/dist/CRUD-project/index.html'));
 });
 app.listen(process.env.PORT || 8080);
